8f5494f18e34cda855ec81e576f6fd8ac2645ad1
Sam Marks <sam@sammarks.me>
2019-04-09 16:07:39 -0400

    feat: Allow additional builder modifications.

--- src/orderedPagedRelationQuery.js
+++ src/orderedPagedRelationQuery.js
@@ -1,4 +1,4 @@
-const orderedPagedRelationQuery = function (parent, field, first, after, args, eager = null) {
+const orderedPagedRelationQuery = function (parent, field, first, after, args, eager = null, modify = null) {
   const { orderBy, orderDirection } = args
   const joinRelation = orderBy.split('.').length > 1 ? orderBy.split('.')[0] : null
   return parent.pagedRelationQuery.call(parent, field, first, after, undefined, (builder) => {
@@ -10,6 +10,9 @@ const orderedPagedRelationQuery = function (parent, field, first, after, args, e
       builder.orderBy(orderBy, orderDirection)
     } else {
       builder.orderBy(`${builder._modelClass.tableName}.${orderBy}`, orderDirection)
+    }
+    if (modify) {
+      modify(builder)
     }
   })
 }
